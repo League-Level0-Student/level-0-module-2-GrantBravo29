c023b61eb7cc7acab6842f99420af6585b0ff62e
elladem <47702293+zzmimicoder@users.noreply.github.com>
2019-04-07 11:27:55 -0700

    more stuff
    :smirk:

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -8,20 +8,25 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class Validation {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args){
+		for (int i = 0; i <10; i++) {
+			
+		}
 		Random randomMaker = new Random();
 		
 		int randomNumber = randomMaker.nextInt(10);
 		if( randomNumber == 0) {
-			
-			JOptionPane.showMessageDialog(null, "Don’t you love nature, despite what it did to you?");
-		}
-		System.out.println(randomNumber);
-
 		
-	
-	
+			
+		
+	}
+		else if(randomNumber == 1) {
+		System.out.println(randomNumber);
+		JOptionPane.showMessageDialog(null, "I remember now, your pyschatrist told you you were stupid and you asked for a second opinion. she said: oh, your ugly too");
+		JOptionPane.showMessageDialog(null, "Don’t you love nature, despite what it did to you?");
+		JOptionPane.showMessageDialog(null, "big cheese is smaller than you");
+	JOptionPane.showMessageDialog(null, "when you swim with the whales they say, WE ARE FAMILY, EVEN THOUGH YOURE BIGGER THAN ME");
+		}
 	
 	
 	
@@ -31,26 +36,8 @@ public class Validation {
 	
 	
 		//Don’t you love nature, despite what it did to you?
+        //I remember now. your pyschatrist told you you were stupid and you asked for a second opinion. she said: oh, your ugly too
 
-				//Don’t think, it might sprain your brain.
-
-				//Fellows like you don’t grow from trees, they swing from them.
-
-				//He has a mechanical mind. Too bad he forgot to wind it up this morning.
-
-				//You’re a man of the world. And you know what sad shape the world is in.
-
-				//He’s always lost in thought. It’s unfamiliar territory.
-
-				//Are you always so stupid or is today a special occasion?
-
-				//He’s listed in Who’s Who as What’s That?
-
-				//He’s living proof that man can live without a brain.
-
-				//He’s so short, when it rains he’s always the last to know.
-	
-	
 	
 	
 	
